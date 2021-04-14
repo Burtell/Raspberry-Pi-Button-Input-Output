c3066ad753d683e754453e455f3a1ab061031754
Burtell <82069514+Burtell@users.noreply.github.com>
2021-04-14 16:52:04 -0400

    Update Using GPIO Pins to Activate LED Lights.py

--- Using GPIO Pins to Activate LED Lights.py
+++ Using GPIO Pins to Activate LED Lights.py
@@ -30,20 +30,24 @@ close_led = LED(19)
 connect_led = LED(26)
 disconnect_led = LED(12)
 
-# if either racking buttons are pressed (so these are actually supposed to activate when the button is held
-# for 3 seconds and deactivate when an outside signal is received)
+# if either racking buttons are pressed (so these activate when the button is held for 3 seconds and deactivate when an outside signal is received)
 while racking_in.when_held:
     ledTimer(racking_led, 3)
-
+#i would assume some code needs to go here to tell it when to shut off the LED
 while racking_out.when_held:
     ledTimer(racking_led, 3)
-
-racking_in.when_activated = ledTimer(racking_led, 3)
-
+    
+#holding both the permissive and the close should activate this sequence
 if permissive.when_held and power_on.when_held:
     close_led.on()
 
-#power_on button press
+#when the open button is pressed, this LED should come on (but for how long?)
+power_off.when_pressed = open_led.on
+
+#need to add a stop LED or figure out how the light will be activated when power is sent to the button
+e_stop.when_released = stop.off
+e_stop.when_pressed = stop.on
+
 pause()
 
 
